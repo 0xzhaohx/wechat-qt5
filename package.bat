2e7b70fab52677bc21404000577b3ab0f130b32a
zhaohx3 <zhaohx4@lenovo.com>
2018-12-07 12:14:34 +0800

    modify pyinstaller config file

--- package.bat
+++ package.bat
@@ -1,4 +1,5 @@
-set PY_INSTALL_HOME=D:\PyInstaller-3.3.1\pyinstaller
-set PATH=%PATH%%PY_INSTALL_HOME% 
-D:\PyInstaller-3.3.1\pyinstaller.py wechat.spec
+rem set PY_INSTALL_HOME=D:\PyInstaller-3.3.1\pyinstaller
+rem set PATH=%PATH%%PY_INSTALL_HOME% 
+rem D:\PyInstaller-3.3.1\pyinstaller.py wechat.spec
+pyinstaller wechat-x86_64.spec
 pause
